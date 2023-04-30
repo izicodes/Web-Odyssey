3a1cafd4900f2af1d06d9e244a73fc875892b472
Isabelle <loariyafiaba@gmail.com>
2023-04-30 19:44:02 +0100

    Working on the Tips pop up

--- src/scripts/login-page.js
+++ src/scripts/login-page.js
@@ -23,3 +23,39 @@ cancelBtn.addEventListener("click", function () {
   passwordInput.value = "";
   usernameInput.value = "";
 });
+
+//  >>> Tips Pop Up <<<
+let wrapper = document.querySelector(".wrapper");
+let tipDiv = document.createElement("div");
+let tipBtn = document.querySelector("#tipBtn");
+let tipOKBtn = document.querySelector("#tipOKBtn");
+
+tipBtn.addEventListener("click", function () {
+  // TODO: If the popup is already up, disbale the other buttons e.g. OK, Cancel and Tip button
+
+  tipDiv.style.position = "absolute";
+  tipDiv.id = "tipsPopUp";
+  tipDiv.setAttribute("class", "gui-wrapper nborder show");
+
+  tipDiv.innerHTML = `<div class="gui-header"><h3 class="gui-htitle">Windows Tips</h3><div class="controls-wrap"><p class="nthinborder controls" id="tipsXControl">✕</p></div></div><div class="tipsPopUp-body flexcolcc"><p>Tip: The username is 'Web Odyssey' and the password is 'password123'.</p><button type="button" class="nthinborder nbthin-shadow gui-btn" id="tipOKBtn">OK</button></div>`;
+
+  wrapper.appendChild(tipDiv);
+});
+
+tipOKBtn.addEventListener('click', function () {
+  tipDiv.remove();
+})
+
+
+// let tipOKBtn = document.querySelector("#tipOKBtn");
+// let tipBtn = document.querySelector("#tipBtn");
+
+let popup = document.getElementById("tipsPopUp");
+let elements = document.querySelectorAll("div:not(#tipsPopUp)");
+
+if (popup.classList.contains("show")) {
+  elements.forEach(function (element) {
+    element.style.pointerEvents = "none";
+    element.style.cursor = "var(--error)";
+  });
+}
